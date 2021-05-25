0c7e292c48d116aa1c5495a2d0bb21b9791c0088
girijapj03 <72901278+girijapj03@users.noreply.github.com>
2021-05-25 18:46:16 +0530

    Update AlphabetZ.java

--- AlphabetZ.java
+++ AlphabetZ.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class AlphabetS{
+class AlphabetZ{
  public static void main( String[] args) {
 	 Scanner s =new Scanner (System.in);
   System.out.println("Enter the num of row");
